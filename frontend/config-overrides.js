ba713d6192901f32fdfa94740e0d61fc904287e2
Aliaksandr Baradach <aliaksandrbaradach@cloud.upwork.com>
2022-05-21 23:11:13 +0200

    Add react-app-rewired to modify CRA webpack config

---
+++ frontend/config-overrides.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  webpack: function(config, env) {
+    config.resolve = {
+      ...config.resolve,
+      alias: { '~': path.resolve(__dirname, 'src') },
+    };
+    return config;
+  },
+};
+
